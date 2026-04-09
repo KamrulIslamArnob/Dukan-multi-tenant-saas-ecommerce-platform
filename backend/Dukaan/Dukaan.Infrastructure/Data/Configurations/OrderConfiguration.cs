7485767f262aac8d10e0795ed079edf633fdee40
Kamru <kamru@dukaan.dev>
2026-04-09 09:00:00 +0600

    feat(infra): add EF Core DbContext and configurations

---
+++ backend/Dukaan/Dukaan.Infrastructure/Data/Configurations/OrderConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace Dukaan.Infrastructure.Data.Configurations;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Dukaan.Domain.Entities;
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.HasKey(o => o.Id);
+        builder.Property(o => o.OrderNumber).IsRequired().HasMaxLength(50);
+        builder.HasIndex(o => o.OrderNumber).IsUnique();
+        builder.Property(o => o.TotalAmount).HasPrecision(18, 2);
+        builder.HasMany(o => o.Items).WithOne().HasForeignKey(i => i.OrderId);
+        builder.HasQueryFilter(o => !o.IsDeleted);
+    }
+}
---
+++ backend/Dukaan/Dukaan.Infrastructure/Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace Dukaan.Infrastructure.Data.Configurations;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Dukaan.Domain.Entities;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.HasKey(p => p.Id);
+        builder.Property(p => p.Name).IsRequired().HasMaxLength(200);
+        builder.Property(p => p.Slug).IsRequired().HasMaxLength(200);
+        builder.Property(p => p.Price).HasPrecision(18, 2);
+        builder.HasIndex(p => p.Slug).IsUnique();
+        builder.HasQueryFilter(p => !p.IsDeleted);
+    }
+}
